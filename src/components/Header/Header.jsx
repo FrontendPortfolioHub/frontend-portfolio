0a1c55e97b2927c9095950e25feb7d8401cd4128
Pavel Maistrenko <maystrenko.p@gmail.com>
2024-11-29 13:22:52 +0100

    implement navbar structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,23 @@
+import { NavLink } from 'react-router-dom';
+import { navItems } from '../../data/navItems';
 import './Header';
 
 export const Header = () => {
   return (
     <header className="header">
       <div className="container">
-        Header
+        <nav class="navbar">
+          <ul class="navbar__list">
+            {navItems.map(item => (
+              <li key={item.id} class="navbar__item">
+                <NavLink to={item.href} className={({ isActive }) => (isActive ? "navbar__link navbar__link--active" : "navbar__link")}>
+                  {item.label}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </nav>
       </div>
     </header>
-  )
-}
+  );
+};
