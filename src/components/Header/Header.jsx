5cb18b4107a2e6cb96b863543f729f4cfe54c21a
Pavel Maistrenko <maystrenko.p@gmail.com>
2024-11-29 14:03:42 +0100

    added switcher buttons

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,17 +6,26 @@ export const Header = () => {
   return (
     <header className="header">
       <div className="container">
+        <button className="language-switcher"></button>
         <nav class="navbar">
           <ul class="navbar__list">
-            {navItems.map(item => (
+            {navItems.map((item) => (
               <li key={item.id} class="navbar__item">
-                <NavLink to={item.href} className={({ isActive }) => (isActive ? "navbar__link navbar__link--active" : "navbar__link")}>
+                <NavLink
+                  to={item.href}
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'navbar__link navbar__link--active'
+                      : 'navbar__link'
+                  }
+                >
                   {item.label}
                 </NavLink>
               </li>
             ))}
           </ul>
         </nav>
+        <button className="theme-switcher"></button>
       </div>
     </header>
   );
