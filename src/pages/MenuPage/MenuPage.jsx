4c346cc410d6f544e7f75db4ed807d3ef1161216
Pavel Maistrenko <maystrenko.p@gmail.com>
2024-12-01 22:52:45 +0100

    implement Menu page

--- src/pages/MenuPage/MenuPage.jsx
+++ src/pages/MenuPage/MenuPage.jsx
@@ -11,45 +11,45 @@ export const MenuPage = () => {
     <div className="menu-page">
       <nav className="mobile__nav">
         <ul className="mobile__nav-list">
-          <li className="nav__item">
+          <li className="mobile__nav-item">
             <NavLink
               to="/about"
-              className="nav__link"
+              className="mobile__nav-link"
               onClick={handleMenuState}
             >
               About
             </NavLink>
           </li>
-          <li className="nav__item">
+          <li className="mobile__nav-item">
             <NavLink
               to="/projects"
-              className="nav__link"
+              className="mobile__nav-link"
               onClick={handleMenuState}
             >
               Projects
             </NavLink>
           </li>
-          <li className="nav__item">
+          <li className="mobile__nav-item">
             <NavLink
               to="/contacts"
-              className="nav__link"
+              className="mobile__nav-link"
               onClick={handleMenuState}
             >
               Contacts
             </NavLink>
           </li>
-          <li className="nav__item">
+          <li className="mobile__nav-item">
             <NavLink
               to="/"
-              className="nav__link"
+              className="mobile__nav-link"
             >
               Home
             </NavLink>
           </li>
-          <li className="nav__item">
+          <li className="mobile__nav-item">
             <NavLink
               to="https://drive.google.com/file/d/1gn8pSq_oSFfB7OU71VvLzV25nurwP1qr/view?usp=drive_link"
-              className="nav__link nav__link--button"
+              className="mobile__nav-link mobile__nav-button"
               target="_blank"
               rel="noreferrer"
             >
