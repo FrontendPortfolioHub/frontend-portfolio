554fbb85644313faa8c26bc53d6d9c242717eebc
Pavel Maistrenko <maystrenko.p@gmail.com>
2024-12-09 13:37:00 +0100

    added button more

---
+++ src/data/homePageNavItems.js
@@ -0,0 +1,5 @@
+export const homePageNavItems = [
+  { id: 1, label: "about me", href: "/about" },
+  { id: 2, label: "projects", href: "/projects" },
+  { id: 3, label: "contacts", href: "/contacts" },
+];
