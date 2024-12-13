ea7c0defd73a082e52fd34d2e7be430acc3c21e4
Pavel Maistrenko <maystrenko.p@gmail.com>
2024-12-13 21:44:40 +0100

    implement contacts page

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -13,19 +13,24 @@ export const Contacts = () => {
         <ul className="contacts-list">
           <li className="contacts-item">
             <p className="contacts-title text-accent">Call me...</p>
-            <a href="tel:+4916096294939" className="contacts-link text-secondary">
+            <a
+              href="tel:+4916096294939"
+              className="contacts-link text-secondary"
+            >
               +4916096294939
             </a>
           </li>
           <li className="contacts-item">
-            <p className="contacts-title text-accent">Chat me in telegram...</p>
+            <p className="contacts-title text-accent">
+              Chat with me on Telegram...
+            </p>
             <a
-              href="https://t.me/@Paul_Master"
+              href="https://t.me/@Pavlo_Maistrenko"
               target="_blank"
               rel="noopener noreferrer"
               className="contacts-link text-secondary"
             >
-              @Paul_Master
+              @Pavlo_Maistrenko
             </a>
           </li>
           <li className="contacts-item">
@@ -43,7 +48,36 @@ export const Contacts = () => {
             <p className="contacts-title text-accent">
               or use the form below...
             </p>
-            <form action=""></form>
+            <form action="/submit" method="post" className="contact-form">
+              <label htmlFor="name" />
+              <input
+                type="text"
+                id="name"
+                name="name"
+                placeholder="Your name..."
+                required
+              />
+
+              <label htmlFor="email" />
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Your e-mail..."
+                required
+              />
+
+              <label htmlFor="message" />
+              <textarea
+                id="message"
+                name="message"
+                placeholder="Your message..."
+                rows="5"
+                required
+              ></textarea>
+
+              <button type="submit" className="contact-form__button">Send...</button>
+            </form>
           </li>
         </ul>
       </div>
