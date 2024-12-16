752ed384375c8c30bda33334b6fc1cd1f88ec27b
Pavel Maistrenko <maystrenko.p@gmail.com>
2024-12-16 12:58:54 +0100

    implement creator info block

---
+++ src/components/CreaterInfo/CreaterInfo.jsx
@@ -0,0 +1,9 @@
+import './CreaterInfo';
+
+export const CreaterInfo = () => {
+  return (
+    <div className="creater-info__content">
+      <p className="creater-info__text">Designed and created by Pavlo Maistrenko, 2024.</p>
+    </div>
+  );
+}
